f413b0a374b908b1a1a40d9577e8e916e83e139c
David Alexandru <david.alexandru.pop2002@gmail.com>
2026-07-13 22:16:14 +0300

    added: day6 part 2

--- 2025/day6/main.go
+++ 2025/day6/main.go
@@ -50,7 +50,90 @@ func solvePart1(fileName string) {
 	fmt.Println("Total Sum:", totalSum)
 }
 
+func makeLinesEqualLength(lines []string) []string {
+	maxLength := 0
+	for _, line := range lines {
+		if len(line) > maxLength {
+			maxLength = len(line)
+		}
+	}
+
+	for i, line := range lines {
+		if len(line) < maxLength {
+			lines[i] = line + strings.Repeat(" ", maxLength-len(line))
+		}
+	}
+
+	return lines
+}
+
+func solvePart2(fileName string) {
+	contentBytes, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(contentBytes)
+	lines := strings.Split(content, "\n")
+	lines = makeLinesEqualLength(lines)
+	operators := strings.Fields(lines[len(lines)-1])
+	indexOperators := 0
+	ans := 0
+	var currentNumbers []int
+	for column := 0; column < len(lines[0]); column++ {
+		allEmpty := true
+		currentNumber := 0
+		for row := 0; row < len(lines)-1; row++ {
+			if lines[row][column] != ' ' {
+				allEmpty = false
+				number := 0
+				if value, err := strconv.Atoi(string(lines[row][column])); err == nil {
+					number = value
+				}
+				currentNumber = currentNumber*10 + number
+			}
+		}
+		if allEmpty {
+			total := 0
+			if operators[indexOperators] == "+" {
+				for _, number := range currentNumbers {
+					total += number
+				}
+			} else {
+				total = 1
+				for _, number := range currentNumbers {
+					total *= number
+				}
+			}
+			indexOperators++
+			ans += total
+			currentNumbers = []int{}
+		} else {
+			currentNumbers = append(currentNumbers, currentNumber)
+		}
+	}
+
+	if len(currentNumbers) > 0 {
+		total := 0
+		if operators[indexOperators] == "+" {
+			for _, number := range currentNumbers {
+				total += number
+			}
+		} else {
+			total = 1
+			for _, number := range currentNumbers {
+				total *= number
+			}
+		}
+		ans += total
+	}
+
+	fmt.Println(ans)
+
+}
+
 func main() {
 	fileName := "input.txt"
-	solvePart1(fileName)
+	// solvePart1(fileName)
+	solvePart2(fileName)
 }
